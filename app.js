17fb239bd68b66780e228ef42ec1d6604e4aedf6
yaniv208 <Yaniv208@gmail.com>
2022-06-03 11:45:08 +0300

    Added userSchema and userModel

--- app.js
+++ app.js
@@ -1,41 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+// Setting routers
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-// view engine setup
+// view engine setup + favicon
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Match specific endpoints to routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+//TODO: Fix connection to DB
+mongoose.connect('mongodb+srv://yaniv208:Shlomo55@cluster0.8zqxl.mongodb.net/test');
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+// Connect to db
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
+const mongoose = require("mongoose");
 var router = express.Router();
+
+const userSchema = new mongoose.Schema({
+  id:String,
+  firstName:String,
+  lastName:String,
+  birthday:Date,
+  maritalStatus:String
+});
+
+// 'users' is the name of the collection inside the database
+const userModel = mongoose.model('users', userSchema);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// http://localhost..../users/add
+{
+}
 module.exports = router;
